c95d407b54f89516d78b2176cc161c6343d519f0
SMIHAI10 <mihai_aragon@yahoo.com>
2026-04-02 13:02:15 +0100

    Robot working in SIM with SLam and Nav2

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -7,15 +7,16 @@ def generate_launch_description():
     return LaunchDescription([
 
         Node(
-            package='camera_ros',
-            executable='camera_node',
-            name='camera',
+            package='rplidar_ros',
+            executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'format': 'RGB888',
-                'image_size': [800,600],
-                'time_per_frame': [1, 30],
-                'camera_frame_id': 'camera_link_optical'
-                }]
+                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'serial_baudrate': 256000,
+                'inverted': False,
+                'scan_mode': 'Standard'
+            }]
         )
     ])
